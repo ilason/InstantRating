8afdc78e0f2404cb5e25ab3d0077fbd4b189c8ae
Aaron Eliason <aaroneliason@hotmail.com>
2016-03-30 14:33:32 -0600

    Views, Controllers, and Models are in

---
+++ Groups.swift
@@ -0,0 +1,9 @@
+//
+//  Groups.swift
+//  InstantRating
+//
+//  Created by Aaron Eliason on 3/30/16.
+//  Copyright © 2016 IlasonApps. All rights reserved.
+//
+
+import Foundation
---
+++ Rating.swift
@@ -0,0 +1,9 @@
+//
+//  Rating.swift
+//  InstantRating
+//
+//  Created by Aaron Eliason on 3/30/16.
+//  Copyright © 2016 IlasonApps. All rights reserved.
+//
+
+import Foundation
