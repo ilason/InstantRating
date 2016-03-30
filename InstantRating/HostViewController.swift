e841c680b8cab32113c71e37a60e339d7d1f0d61
Aaron Eliason <aaroneliason@hotmail.com>
2016-03-30 15:40:56 -0600

    Alert Controllers Working With Table Views

---
+++ InstantRating/HostViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  InstantRating
+//
+//  Created by Aaron Eliason on 3/30/16.
+//  Copyright © 2016 IlasonApps. All rights reserved.
+//
+
+import UIKit
+
+class HostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func createGroupButtonTapped(sender: AnyObject) {
+        var loginTextField: UITextField?
+        let alertController = UIAlertController(title: "Create Group", message: "Please Enter Group ID.", preferredStyle: .Alert)
+        let ok = UIAlertAction(title: "Create", style: .Default, handler: { (action) -> Void in
+            print("Create Button Pressed")
+        })
+        let cancel = UIAlertAction(title: "Cancel", style: .Cancel) { (action) -> Void in
+            print("Cancel Button Pressed")
+        }
+        alertController.addAction(ok)
+        alertController.addAction(cancel)
+        alertController.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            // Enter the textfiled customization code here.
+            loginTextField = textField
+            loginTextField?.placeholder = "Enter Group ID"
+        }
+        presentViewController(alertController, animated: true, completion: nil)
+
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("hostGroupsCell", forIndexPath: indexPath)
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+
+}
---
+++ InstantRating/JuryViewController.swift
@@ -0,0 +1,74 @@
+//
+//  JuryHostViewController.swift
+//  InstantRating
+//
+//  Created by Aaron Eliason on 3/30/16.
+//  Copyright © 2016 IlasonApps. All rights reserved.
+//
+
+import UIKit
+
+class JuryViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func joinGroupButtonTapped(sender: AnyObject) {
+        
+        var loginTextField: UITextField?
+        let alertController = UIAlertController(title: "Join Group", message: "Please Enter Group ID Accurately.", preferredStyle: .Alert)
+        let ok = UIAlertAction(title: "Join", style: .Default, handler: { (action) -> Void in
+            print("Ok Button Pressed")
+        })
+        let cancel = UIAlertAction(title: "Cancel", style: .Cancel) { (action) -> Void in
+            print("Cancel Button Pressed")
+        }
+        alertController.addAction(ok)
+        alertController.addAction(cancel)
+        alertController.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            // Enter the textfiled customization code here.
+            loginTextField = textField
+            loginTextField?.placeholder = "Enter Group ID"
+        }
+        presentViewController(alertController, animated: true, completion: nil)
+        
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+   
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("juryGroupCell", forIndexPath: indexPath)
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
